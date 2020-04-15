da59f7ef2237c898137e61a02631fca93365edb9
iamgoofball <iamgoofball@gmail.com>
2020-04-15 00:57:44 -0700

    Commits the source code.

---
+++ winforms-tacotron-transcription/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace winforms_tacotron_transcription
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProgressBarTranscripts = new System.Windows.Forms.ProgressBar();
+            this.ProgressLabel = new System.Windows.Forms.Label();
+            this.TranscriptionBox = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.WavName = new System.Windows.Forms.Label();
+            this.SaveTranscriptionButton = new System.Windows.Forms.Button();
+            this.NextLineButton = new System.Windows.Forms.Button();
+            this.PreviousLineButton = new System.Windows.Forms.Button();
+            this.PlayAudioButton = new System.Windows.Forms.Button();
+            this.NewTranscriptionButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ProgressBarTranscripts
+            // 
+            this.ProgressBarTranscripts.Location = new System.Drawing.Point(12, 406);
+            this.ProgressBarTranscripts.Maximum = 420;
+            this.ProgressBarTranscripts.Name = "ProgressBarTranscripts";
+            this.ProgressBarTranscripts.Size = new System.Drawing.Size(556, 23);
+            this.ProgressBarTranscripts.TabIndex = 0;
+            this.ProgressBarTranscripts.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // ProgressLabel
+            // 
+            this.ProgressLabel.AutoSize = true;
+            this.ProgressLabel.Location = new System.Drawing.Point(9, 390);
+            this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.Size = new System.Drawing.Size(162, 13);
+            this.ProgressLabel.TabIndex = 1;
+            this.ProgressLabel.Text = "Transcription Progress: 0 of 0 left";
+            this.ProgressLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TranscriptionBox
+            // 
+            this.TranscriptionBox.Enabled = false;
+            this.TranscriptionBox.Location = new System.Drawing.Point(12, 44);
+            this.TranscriptionBox.Name = "TranscriptionBox";
+            this.TranscriptionBox.Size = new System.Drawing.Size(556, 343);
+            this.TranscriptionBox.TabIndex = 2;
+            this.TranscriptionBox.Text = "He turned himself into a pickle. He called himself \"Pickle Rick\". It was the funn" +
+    "iest shit I\'ve ever seen.";
+            this.TranscriptionBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Current File:";
+            // 
+            // WavName
+            // 
+            this.WavName.AutoSize = true;
+            this.WavName.Enabled = false;
+            this.WavName.Location = new System.Drawing.Point(9, 28);
+            this.WavName.Name = "WavName";
+            this.WavName.Size = new System.Drawing.Size(74, 13);
+            this.WavName.TabIndex = 6;
+            this.WavName.Text = "ass_blast_usa";
+            this.WavName.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // SaveTranscriptionButton
+            // 
+            this.SaveTranscriptionButton.Enabled = false;
+            this.SaveTranscriptionButton.Location = new System.Drawing.Point(575, 73);
+            this.SaveTranscriptionButton.Name = "SaveTranscriptionButton";
+            this.SaveTranscriptionButton.Size = new System.Drawing.Size(213, 23);
+            this.SaveTranscriptionButton.TabIndex = 8;
+            this.SaveTranscriptionButton.Text = "Save Transcription File";
+            this.SaveTranscriptionButton.UseVisualStyleBackColor = true;
+            this.SaveTranscriptionButton.Click += new System.EventHandler(this.SaveTranscriptionButton_Click);
+            // 
+            // NextLineButton
+            // 
+            this.NextLineButton.Enabled = false;
+            this.NextLineButton.Location = new System.Drawing.Point(574, 406);
+            this.NextLineButton.Name = "NextLineButton";
+            this.NextLineButton.Size = new System.Drawing.Size(213, 23);
+            this.NextLineButton.TabIndex = 9;
+            this.NextLineButton.Text = "Next line (Pg Down)";
+            this.NextLineButton.UseVisualStyleBackColor = true;
+            this.NextLineButton.Click += new System.EventHandler(this.NextLineButton_Click);
+            // 
+            // PreviousLineButton
+            // 
+            this.PreviousLineButton.Enabled = false;
+            this.PreviousLineButton.Location = new System.Drawing.Point(574, 377);
+            this.PreviousLineButton.Name = "PreviousLineButton";
+            this.PreviousLineButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.PreviousLineButton.Size = new System.Drawing.Size(213, 23);
+            this.PreviousLineButton.TabIndex = 10;
+            this.PreviousLineButton.Text = "Previous line (Pg Up)";
+            this.PreviousLineButton.UseVisualStyleBackColor = true;
+            this.PreviousLineButton.Click += new System.EventHandler(this.PreviousLineButton_Click);
+            // 
+            // PlayAudioButton
+            // 
+            this.PlayAudioButton.Enabled = false;
+            this.PlayAudioButton.Location = new System.Drawing.Point(575, 102);
+            this.PlayAudioButton.Name = "PlayAudioButton";
+            this.PlayAudioButton.Size = new System.Drawing.Size(213, 266);
+            this.PlayAudioButton.TabIndex = 11;
+            this.PlayAudioButton.Text = "Play Audio (END)";
+            this.PlayAudioButton.UseVisualStyleBackColor = true;
+            this.PlayAudioButton.Click += new System.EventHandler(this.PlayAudioButton_Click);
+            // 
+            // NewTranscriptionButton
+            // 
+            this.NewTranscriptionButton.Location = new System.Drawing.Point(575, 44);
+            this.NewTranscriptionButton.Name = "NewTranscriptionButton";
+            this.NewTranscriptionButton.Size = new System.Drawing.Size(213, 23);
+            this.NewTranscriptionButton.TabIndex = 12;
+            this.NewTranscriptionButton.Text = "New Transcription Project";
+            this.NewTranscriptionButton.UseVisualStyleBackColor = true;
+            this.NewTranscriptionButton.Click += new System.EventHandler(this.NewTranscriptionButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NewTranscriptionButton);
+            this.Controls.Add(this.PlayAudioButton);
+            this.Controls.Add(this.PreviousLineButton);
+            this.Controls.Add(this.NextLineButton);
+            this.Controls.Add(this.SaveTranscriptionButton);
+            this.Controls.Add(this.WavName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TranscriptionBox);
+            this.Controls.Add(this.ProgressLabel);
+            this.Controls.Add(this.ProgressBarTranscripts);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Tacotron Transcription File Maker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar ProgressBarTranscripts;
+        private System.Windows.Forms.Label ProgressLabel;
+        private System.Windows.Forms.RichTextBox TranscriptionBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label WavName;
+        private System.Windows.Forms.Button SaveTranscriptionButton;
+        private System.Windows.Forms.Button NextLineButton;
+        private System.Windows.Forms.Button PreviousLineButton;
+        private System.Windows.Forms.Button PlayAudioButton;
+        private System.Windows.Forms.Button NewTranscriptionButton;
+    }
+}
+
---
+++ winforms-tacotron-transcription/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Diagnostics;
+
+namespace winforms_tacotron_transcription
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<String, String> transcriptions = new Dictionary<string, string>();
+        public int current_line_number = 0;
+        public string folder_path = "";
+        public SoundPlayer player;
+        public bool loaded_shit = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            player = new SoundPlayer();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void PlayAudioButton_Click(object sender, EventArgs e)
+        {
+            player.SoundLocation = folder_path + "\\" + transcriptions.ElementAt(current_line_number).Key;
+            player.Load();
+            player.Play();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewTranscriptionButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            folderBrowser.FileName = "Navigate to folder with WAV files";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = Path.GetDirectoryName(folderBrowser.FileName);
+                folder_path = folderPath;
+                string[] files = Directory.GetFiles(folderPath, "*.wav", SearchOption.TopDirectoryOnly);
+                if (files.Length == 0)
+                    return;
+                transcriptions = new Dictionary<string, string>();
+                foreach (string item in files)
+                {
+                    transcriptions.Add(Path.GetFileName(item), "");
+                }
+                current_line_number = 0;
+                SaveTranscriptionButton.Enabled = true;
+                PlayAudioButton.Enabled = true;
+                NextLineButton.Enabled = true;
+                PreviousLineButton.Enabled = true;
+                TranscriptionBox.Enabled = true;
+                WavName.Enabled = true;
+                loaded_shit = true;
+                ChangeLineInBox();
+                UpdateProgressBar();
+                SaveTranscriptionLine();
+            }
+        }
+        public void SaveTranscriptionLine()
+        {
+            transcriptions[transcriptions.ElementAt(current_line_number).Key] = TranscriptionBox.Text;
+        }
+        public void ChangeLine(bool up_or_down)
+        {
+            SaveTranscriptionLine();
+            if (up_or_down)
+            {
+                if(current_line_number < transcriptions.Count())
+                {
+                    current_line_number++;
+                    ChangeLineInBox();
+                    UpdateProgressBar();
+                }
+            }
+            else
+            {
+                if (current_line_number > 0)
+                {
+                    current_line_number--;
+                    ChangeLineInBox();
+                    UpdateProgressBar();
+                }
+            }
+        }
+        public void UpdateProgressBar()
+        {
+            ProgressBarTranscripts.Maximum = transcriptions.Count();
+            ProgressBarTranscripts.Value = current_line_number;
+            int current_num = ProgressBarTranscripts.Value + 1;
+            int max_num = transcriptions.Count();
+            ProgressLabel.Text = "Transcription Progress: " + current_num.ToString() + " of " + max_num.ToString();
+        }
+        public void ChangeLineInBox()
+        {
+            TranscriptionBox.Text = transcriptions.ElementAt(current_line_number).Value;
+            WavName.Text = transcriptions.ElementAt(current_line_number).Key;
+        }
+
+        private void NextLineButton_Click(object sender, EventArgs e)
+        {
+            ChangeLine(true);
+        }
+
+        private void PreviousLineButton_Click(object sender, EventArgs e)
+        {
+            ChangeLine(false);
+        }
+
+        private void LoadTranscriptionButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveTranscriptionButton_Click(object sender, EventArgs e)
+        {
+            SaveTranscriptionLine();
+            save_file();
+        }
+        public void save_file()
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    using (StreamWriter file = new StreamWriter(myStream))
+                        foreach (var entry in transcriptions)
+                            file.WriteLine("wavs/out/{0}|{1};", entry.Key, entry.Value);
+                    myStream.Close();
+                }
+            }
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.PageDown && loaded_shit)
+            {
+                ChangeLine(true);
+                return true;
+            }
+            if(keyData == Keys.PageUp && loaded_shit)
+            {
+                ChangeLine(false);
+                return true;
+            }
+            if (keyData == Keys.End && loaded_shit)
+            {
+                player.SoundLocation = folder_path + "\\" + transcriptions.ElementAt(current_line_number).Key;
+                player.Load();
+                player.Play();
+                return true;
+            }
+            if(keyData == (Keys.Control | Keys.S) && loaded_shit)
+            {
+                SaveTranscriptionLine();
+                save_file();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
